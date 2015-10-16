f7d92ac005e13a6b7ccae1bdf6ab44f7f9c1fd35
hakushu.io <hakushu.publishing@gmail.com>
2015-10-16 22:16:43 +0300

    Update install_script2.sh

--- install_script2.sh
+++ install_script2.sh
@@ -5,6 +5,8 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 \curl -sSL https://get.rvm.io | bash
 /usr/local/rvm/bin/rvm requirements
 
+rvm implode
+
 # NGINX
 
 cd /tmp
